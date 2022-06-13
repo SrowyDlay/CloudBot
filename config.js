656fe573859bacae5391e0a3020ffe99e2a8d3ce
SrowyDlay <61784424+SrowyDlay@users.noreply.github.com>
2022-06-13 14:53:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,9 @@ global.sgh = process.env.LINK_GITHUB
 global.sgc = process.env.LINK_GROUP
 global.ownernumber = process.env.NOWNER
 global.owner = [
+  ['6285788029733'],
   ['0'],
-  ['0'],
+  ['6285788029733', 'ELLZY' true],
   [`${process.env.NOWNER}`, `${process.env.NAMAOWNER}`, true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
